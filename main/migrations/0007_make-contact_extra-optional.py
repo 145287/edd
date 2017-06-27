5377a99899242e7cde747baad136817b92534d3e
Mark Forrer <mark.forrer@lbl.gov>
2017-06-27 12:22:24 -0700

    Fixed migration sequence problem resulting from merge

--- main/migrations/0007_make-contact_extra-optional.py
+++ main/migrations/0007_make-contact_extra-optional.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('main', '0005_default-sbml-template'),
+        ('main', '0006_add-protein-strain-link'),
     ]
 
     operations = [
